75c71edfafc482fade3cd9cf027dfeced375284c
Keks <keks@htmlacademy.ru>
2022-07-11 13:39:04 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,21 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if(min >= max){
+    console.log('false');
+  } else {
+    return Math.floor(Math.random()*(max-min+1)+min)
+  }
+}
+
+    // функцию нашла на https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+    function getLenghtCount(string, lenght) {
+      if (string.length > lenght){
+        return false;
+        } else {
+        return true;
+          }
+    }
+   //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length
+
