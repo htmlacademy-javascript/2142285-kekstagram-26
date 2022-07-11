1b3e45f592db01f11499cf3cba6b1899ec93e679
irok1222 <irokcheese@mail.ru>
2022-07-11 13:20:48 +0300

    Добавляем функции

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    if(min >= max){
-        console.log('ошибка')
-    } else {
-    return Math.floor(Math.random() * (max - min + 1)) + min};
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if(min >= max){
+    console.log('false');
+  } else {
+    return Math.floor(Math.random()*(max-min+1)+min)
+  }
 }
+
     // функцию нашла на https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-
 
     function getLenghtCount(string, lenght) {
       if (string.length > lenght){
