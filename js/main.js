6b5f42c6380cdade80ce267609eca0ca2fa693dd
irok1222 <irokcheese@mail.ru>
2022-07-23 15:29:27 +0300

    Нужно больше функции

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if(min>max){
-    throw new Error
+    throw new Error;
   }
-    return Math.floor(Math.random()*(max-min+1)+min)
+    return Math.floor(Math.random()*(max-min+1)+min);
   }
     // функцию нашла на https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-    function getLenghtCount(string, length) {
-      return string.length > length
+function getLengthCount(string, length) {
+  return string.length > length;
     }
     //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length
 
